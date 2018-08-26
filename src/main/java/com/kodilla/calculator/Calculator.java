7bc01439c815b5386e94ab00e6f953a4e6db5427
trumat <matru@protonmail.com>
2018-08-26 11:54:16 +0100

    update

--- src/main/java/com/kodilla/calculator/Calculator.java
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -5,11 +5,11 @@ public class Calculator {
         return valueA + valueB;
     }
 
-    public void substractValues(double valueA, double valueB) { return valueA - valueB; }
+    public double substractValues(double valueA, double valueB) { return valueA - valueB; }
 
     public static void main(String[] args) {
-        Calculator calculator = new Calculator(0);
-        System.out.println(calculator.addValues(2, 4));
-        System.out.println(calculator.substractValues(55.5, 100));
+        Calculator calculator = new Calculator();
+        System.out.println("2 + 4 = " + calculator.addValues(2, 4));
+        System.out.println("55.5 - 100 = " + calculator.substractValues(55.5, 100));
     }
 }
